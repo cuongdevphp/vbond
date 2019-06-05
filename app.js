2c379f892a99800cc954d71b7dd3838c5e9247d4
Cuong Au <cuongah@vcsc.com.vn>
2019-06-05 15:06:44 +0700

    prefix api

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var debug = require('debug')('vbond:server');
+var http = require('http');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var postRouter = require('./routes/post');
 var prefixRouter = require('./routes/prefix');
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+var port = normalizePort(process.env.PORT || '3001');
 
 var app = express();
 // view engine setup
@@ -19,11 +26,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(bodyParser);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/post', postRouter);
 app.use('/prefix', prefixRouter);
 
 // catch 404 and forward to error handler
@@ -41,5 +46,83 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+var hostname = '0.0.0.0';
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 module.exports = app;
--- routes/prefix.js
+++ routes/prefix.js
@@ -1,33 +1,58 @@
 var express = require('express');
 const { poolPromise } = require('../db')
 var router = express.Router();
+const tbl = '[dbo].[TB_PREFIX]';
 
 /* GET prefix listing. */
-
 router.get('/', async (req, res) => {
     try {
         const pool = await poolPromise;
-        const result = await pool.request()
-            .query('SELECT * FROM [dbo].[TB_PREFIX]')
-        res.json(result.recordset);
+        const result = await pool.request().query('SELECT * FROM '+ tbl +'');
+        return res.json(result.recordset);
     } catch (err) {
-        res.status(500)
-        res.send(err.message)
+        res.status(500);
+        res.send(err.message);
     }
 });
 
 router.post('/create', async (req, res) => {
     try {
+        console.log(req.body, "req.body");
         const KYTU_PREFIX = req.body.KYTU_PREFIX;
         const GHICHU = req.body.GHICHU;
         const pool = await poolPromise;
-        const sql = "INSERT INTO [dbo].[TB_PREFIX] (KYTU_PREFIX, GHICHU, FLAG) VALUES ('"+KYTU_PREFIX+"', '"+GHICHU+"', '4')";
+        const sql = "INSERT INTO "+tbl+" (KYTU_PREFIX, GHICHU, FLAG) VALUES ('"+KYTU_PREFIX+"', '"+GHICHU+"', '1')";
         const result = await pool.request().query(sql);
-        res.send('Create data successfully');
+        res.send('Create data successful!');
     } catch (err) {
         res.status(500);
         res.send(err.message);
     }
-})
+});
+
+// router.put('/update', async (req, res) => {
+//     try {
+//         const PREFIX_ID = req.body.PREFIX_ID;
+//         const sql = "UPDATE "+tbl+" SET FLAG = '0' WHERE PREFIX_ID = "+PREFIX_ID+"";
+//         const result = await pool.request().query(sql);
+//         res.send('Delete data successfully');
+//     } catch (err) {
+//         res.status(500);
+//         res.send(err.message);
+//     }
+// });
+
+router.put('/delete', async (req, res) => {
+    try {
+        const PREFIX_ID = req.body.PREFIX_ID;
+        const sql = "UPDATE "+tbl+" SET FLAG = '0' WHERE PREFIX_ID = "+PREFIX_ID+"";
+        const pool = await poolPromise;
+        const result = await pool.request().query(sql);
+        res.send('Delete data successfully');
+    } catch (err) {
+        res.status(500);
+        res.send(err.message);
+    }
+});
 
 module.exports = router;

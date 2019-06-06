a0639bc1bf877f29a37ce0183841972b0ed12688
Cuong Au <cuongah@vcsc.com.vn>
2019-06-06 15:01:53 +0700

    fix header

--- routes/prefix.js
+++ routes/prefix.js
@@ -1,13 +1,26 @@
 var express = require('express');
-var app = express();
-var cors = require('cors');
-app.use(cors());
 const { poolPromise } = require('../db')
 var router = express.Router();
 const tbl = '[dbo].[TB_PREFIX]';
 
 /* GET prefix listing. */
 router.get('/', async (req, res) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+
     try {
         const pool = await poolPromise;
         const result = await pool.request().query('SELECT * FROM '+ tbl +'');

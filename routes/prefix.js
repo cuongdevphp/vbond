5a805a59ad070209bc9652a4ebf72d2adf2c0a45
Cuong Au <cuongah@vcsc.com.vn>
2019-06-06 15:03:59 +0700

    fix header

--- routes/prefix.js
+++ routes/prefix.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const tbl = '[dbo].[TB_PREFIX]';
 
 /* GET prefix listing. */
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
       // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
@@ -17,9 +17,6 @@ router.get('/', async (req, res, next) => {
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
 
     try {
         const pool = await poolPromise;

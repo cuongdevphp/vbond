ac822cf7e43e40b1a33d8708c5b9420b6d6c798a
Cuong Au <cuongah@vcsc.com.vn>
2019-06-06 14:42:00 +0700

    fix header

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var prefixRouter = require('./routes/prefix');
 var companyRouter = require('./routes/company');
-
+const cors = require('cors');
 
 /**
  * Get port from environment and store in Express.
@@ -33,8 +33,28 @@ app.use('/users', usersRouter);
 app.use('/prefix', prefixRouter);
 app.use('/company', companyRouter);
 
-var cors = require('cors');
 app.use(cors());
+app.options('*', cors());
+
+// // Add headers
+// app.all('/*', function(req, res,next) {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

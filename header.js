ad73eeffa06611de055cf1c1576fc5ba440a3781
Cuong Au <cuongah@vcsc.com.vn>
2019-06-12 09:39:48 +0700

    fee api

--- header.js
+++ header.js
@@ -1,17 +1,33 @@
+var jwt = require('jsonwebtoken');
+
 module.exports = {
-    setHeader: function (res) {
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', '*');
+    verifyToken: (req, res, next) => {
+        // FORMAT OF TOKEN
+        // Authorization: Bearer <access_token>
+        // Get auth header value
+        const bearerHeader = req.headers['authorization'];
+        // Check if bearer is undefined
+        if(typeof bearerHeader !== 'undefined') {
+            // Split at the space
+            const bearer = bearerHeader.split(' ');
+            // Get token from array
+            const bearerToken = bearer[1];
+            // Set the token
+            req.token = bearerToken;
+            // Next middleware
+            next();
+        } else {
+            // Forbidden
+            res.sendStatus(403);
+        }
+    },
 
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
+    jwtVerify: async (req, res) => {
+        jwt.verify(req.token, 'secretkey', (err) => {
+            if(err) {
+                res.status(403).json({ error: err.message });
+            }
+        });
     }
 };
   

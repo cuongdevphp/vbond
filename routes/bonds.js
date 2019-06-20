c2625f48e303116e3c3cac49c38703e72eb26da5
Cuong Au <cuongah@vcsc.com.vn>
2019-06-20 15:58:19 +0700

    fixbug

--- routes/bonds.js
+++ routes/bonds.js
@@ -10,7 +10,6 @@ const tbl_company = '[dbo].[TB_CONGTY]';
 const tbl_KHTT = '[dbo].[TB_KYHANTHANHTOAN]';
 const tbl_bondType = '[dbo].[TB_LOAITRAIPHIEU]';
 const tbl_NTLTN = '[dbo].[TB_NGAYTINHLAITRONGNAM]';
-const tbl_TSDB = '[dbo].[TB_TAISANDAMBAO]';
 const tbl_contractVCSC = '[dbo].[TB_HOPDONGMUA_VCSC]';
 
 /* GET listing. */
@@ -22,7 +21,6 @@ router.get('/', header.verifyToken, async (req, res) => {
                         p.*, 
                         a.SOHD, 
                         b.TEN_DN, 
-                        c.TENTAISANDAMBAO, 
                         d.MSKYHANTT, 
                         e.TENLOAI_TP, 
                         f.SONGAYTINHLAI
@@ -30,7 +28,6 @@ router.get('/', header.verifyToken, async (req, res) => {
                         ${tbl} p
                     LEFT JOIN ${tbl_contractVCSC} a ON a.SOHD = p.SO_HD
                     LEFT JOIN ${tbl_company} b ON b.MSDN = p.MS_DN
-                    LEFT JOIN ${tbl_TSDB} c ON c.MSTSDB = p.MS_TSDB
                     LEFT JOIN ${tbl_KHTT} d ON d.MSKYHANTT = p.MS_KYHANTT
                     LEFT JOIN ${tbl_bondType} e ON e.MSLTP = p.MS_LTP
                     LEFT JOIN ${tbl_NTLTN} f ON f.MSNTLTN = p.MS_NTLTN
@@ -51,7 +48,6 @@ router.post('/', header.verifyToken, async (req, res) => {
         const MSTP = req.body.MSTP;
         const SO_HD = req.body.SO_HD;
         const MS_DN = req.body.MS_DN;
-        const MS_TSDB = req.body.MS_TSDB;
         const MS_KYHANTT = req.body.MS_KYHANTT;
         const MS_LTP = req.body.MS_LTP;
         const MS_NTLTN = req.body.MS_NTLTN;
@@ -72,17 +68,16 @@ router.post('/', header.verifyToken, async (req, res) => {
         const TT_NIEMYET = req.body.TT_NIEMYET;
         const TS_DAMBAO = req.body.TS_DAMBAO;
         const SL_LUUKY = req.body.SL_LUUKY;
-        console.log(req.body);
         const pool = await poolPromise;
         const queryDulicateMSTP = `SELECT MSTP FROM ${tbl} WHERE MSTP = '${MSTP}'`;
         const rsDup = await pool.request().query(queryDulicateMSTP);
         if(rsDup.recordset.length === 0) {
             const sql = `INSERT INTO ${tbl}
-                (MSTP, SO_HD, MS_DN, MS_TSDB, MS_KYHANTT, MS_LTP, MS_NTLTN, LAISUAT_HH, 
+                (MSTP, SO_HD, MS_DN, MS_KYHANTT, MS_LTP, MS_NTLTN, LAISUAT_HH, 
                 MAVIETTAT, TT_TRAIPHIEU, MENHGIA, SL_PHTD, SL_DPH, SL_LH, SL_TH, NGAYPH, 
                 NGAYDH, NGAY_KTPH, TONGHANMUC_HUYDONG, HANMUC_CHO, KYHAN_CONLAI, 
                 TT_NIEMYET, TS_DAMBAO, SL_LUUKY, NGAYTAO, FLAG) VALUES 
-                (N'${MSTP}', N'${SO_HD}', N'${MS_DN}', ${MS_TSDB}, 
+                (N'${MSTP}', N'${SO_HD}', N'${MS_DN}', 
                 N'${MS_KYHANTT}', N'${MS_LTP}', ${MS_NTLTN}, ${LAISUAT_HH}, N'${MAVIETTAT}', 
                 N'${TT_TRAIPHIEU}', ${MENHGIA}, ${SL_PHTD}, ${SL_DPH}, ${SL_LH}, ${SL_TH}, 
                 '${new Date(NGAYPH).toISOString()}', '${new Date(NGAYDH).toISOString()}', 
@@ -110,7 +105,6 @@ router.put('/', header.verifyToken, async (req, res) => {
         const MSTP = req.body.MSTP;
         const SO_HD = req.body.SO_HD;
         const MS_DN = req.body.MS_DN;
-        const MS_TSDB = req.body.MS_TSDB;
         const MS_KYHANTT = req.body.MS_KYHANTT;
         const MS_LTP = req.body.MS_LTP;
         const MS_NTLTN = req.body.MS_NTLTN;
@@ -137,7 +131,6 @@ router.put('/', header.verifyToken, async (req, res) => {
                         MSTP = N'${MSTP}', 
                         SO_HD = N'${SO_HD}', 
                         MS_DN = N'${MS_DN}', 
-                        MS_TSDB = ${MS_TSDB}, 
                         MS_KYHANTT = N'${MS_KYHANTT}', 
                         MS_LTP = N'${MS_LTP}', 
                         MS_NTLTN = ${MS_NTLTN}, 

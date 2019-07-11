81689db9fb89db4ad675780622dd5143f5e18c95
cuongdev <cuongdevphp@gmail.com>
2019-07-11 13:54:37 +0700

    fixbug

--- routes/bonds.js
+++ routes/bonds.js
@@ -146,7 +146,10 @@ router.post('/', header.verifyToken, async (req, res) => {
                 //     (BOND_ID, HANMUC, DANGCHO, THANGCONLAI, TRANGTHAI, NGAYTAO, FLAG) VALUES 
                 //     (${rs.recordset[0].BONDID}, ${TONGHANMUC_HUYDONG}, ${0}, ${month}, ${1}, '${moment().toISOString()}', ${1});
                 // `);
+                console.log(KYHAN);
+                console.log(NGAYPH);
                 console.log(moment(NGAYPH, "DD-MM-YYYY").add((KYHAN * 30), 'days').toISOString());
+                console.log(moment(new Date(new Date(new Date(NGAYPH)).setMonth(new Date(NGAYPH).getMonth()+6))));
                 // await pool.request().query(`
                 //     INSERT INTO ${tbl_interest_rate_buy} 
                 //     (BOND_ID, LS_TOIDA, TRANGTHAI, NGAYBATDAU, NGAYKETTHUC, NGAYTAO, FLAG) VALUES 

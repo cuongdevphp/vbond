3ce1120f652ce9305c2c0c3d2a1e0129274a7bbb
cuongdev <cuongdevphp@gmail.com>
2019-07-11 10:04:25 +0700

    fixbug

--- routes/interest_rate_buy.js
+++ routes/interest_rate_buy.js
@@ -13,7 +13,7 @@ router.get('/', header.verifyToken, async (req, res) => {
     try {
         const pool = await poolPromise;
         const sql = `SELECT
-                p.*, a.MSTP 
+                p.*, a.MSTP, a.BONDID 
             FROM
                 ${tbl} p 
             LEFT JOIN ${tbl_bond} a ON a.BONDID = p.BOND_ID 
@@ -34,22 +34,26 @@ router.post('/', header.verifyToken, async (req, res) => {
         const BOND_ID = req.body.BOND_ID;
         const LS_TOIDA = req.body.LS_TOIDA;
         const LS_BIENDO = req.body.LS_BIENDO;
+        const TRANGTHAI = req.body.TRANGTHAI;
         const DIEUKHOAN_LS = req.body.DIEUKHOAN_LS || '';
-        
-        const queryDulicateMSLS = `SELECT MSLS FROM ${tbl} WHERE MSLS = '${MSLS}'`;
-        const rsDup = await pool.request().query(queryDulicateMSLS);
-        if(rsDup.recordset.length === 0) {
-            const sql = `INSERT INTO ${tbl}
-                (BOND_ID, LS_TOIDA, LS_TH, LS_BIENDO, DIEUKHOAN_LS, NGAYTAO, FLAG) VALUES 
-                (${BOND_ID}, ${LS_TOIDA}, ${LS_TH}, ${LS_BIENDO}, N'${DIEUKHOAN_LS}', '${moment().toISOString()}', ${1});`
-            try {
-                await pool.request().query(sql);
-                res.send('Create data successful!');
-            } catch (error) {
-                res.status(500).json({ error: error.message });
-            }
-        } else {
-            res.status(500).json({ error: 'MSLS bị trùng!'});
+        if(TRANGTHAI === 1) {
+            const rsDup = await pool.request().query(`
+                SELECT MSLS 
+                FROM ${tbl} 
+                WHERE TRANGTHAI = ${1} AND BOND_ID = ${BOND_ID}`
+            );
+            await pool.request().query(`UPDATE ${tbl} SET 
+                TRANGTHAI = ${0}
+            WHERE MSLS = ${rsDup.recordset[0].MSLS}`);
+        }
+        const sql = `INSERT INTO ${tbl}
+            (BOND_ID, LS_TOIDA, LS_BIENDO, DIEUKHOAN_LS, TRANGTHAI, NGAYTAO, FLAG) VALUES 
+            (${BOND_ID}, ${LS_TOIDA}, ${LS_BIENDO}, N'${DIEUKHOAN_LS}', ${TRANGTHAI} '${moment().toISOString()}', ${1});`
+        try {
+            await pool.request().query(sql);
+            res.send('Create data successful!');
+        } catch (error) {
+            res.status(500).json({ error: error.message });
         }
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -70,6 +74,27 @@ router.put('/', header.verifyToken, async (req, res) => {
                         LS_TOIDA = ${LS_TOIDA}, 
                         LS_BIENDO = ${LS_BIENDO}, 
                         DIEUKHOAN_LS = N'${DIEUKHOAN_LS}', 
+                        NGAYUPDATE = '${moment().toISOString()}' 
+                    WHERE MSLS = ${MSLS}`;
+        try {
+            await pool.request().query(sql);
+            res.send('Update data successfully');
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.put('/status', header.verifyToken, async (req, res) => {
+    try {
+        const MSLS = req.body.MSLS;
+        const TRANGTHAI = req.body.TRANGTHAI;
+        
+        const pool = await poolPromise;
+        const sql = `UPDATE ${tbl} SET 
+                        TRANGTHAI = ${TRANGTHAI}, 
                         NGAYUPDATE = '${moment().toISOString()}' 
                     WHERE MSLS = ${MSLS}`;
         try {
